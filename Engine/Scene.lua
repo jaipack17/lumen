c2871f57ab0e302f99208bc5b319ff7876f18b57
jaipack17 <jaikaransingh2007@outlook.com>
2022-04-27 19:31:29 +0530

    Scene.new()

--- Engine/Scene.lua
+++ Engine/Scene.lua
@@ -1,8 +1,38 @@
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local ViewportCanvas = require(script.Parent.Common.ViewportCanvas)
+
 local Scene = {}
 Scene.__index = Scene
 
-function Scene.new(sceneName: string, fullscreen: boolean)
+function Scene.new(resX: number, resY: number, sceneName: string?, fullscreen: boolean)
+	local SceneGui = Instance.new("ScreenGui")
+	SceneGui.IgnoreGuiInset = true
+	SceneGui.ResetOnSpawn = false
+	SceneGui.Name = sceneName or "Scene_"..HttpService:GenerateGUID(false)
 
+	local SceneCanvas = Instance.new("Frame")
+	SceneCanvas.Name = "Canvas"
+	SceneCanvas.Position = UDim2.new(0, 0, 0, 0)
+	SceneCanvas.Size = if fullscreen then UDim2.fromScale(1, 1) else UDim2.new(0, 0, 0, 0)
+	SceneCanvas.BorderSizePixel = 0
+	SceneCanvas.BackgroundColor3 = Color3.new(1, 1, 1)
+	SceneCanvas.Visible = false
+	SceneCanvas.Parent = SceneGui
+
+	SceneGui.Parent = Players.LocalPlayer
+
+	local Canvas = ViewportCanvas.new(resX, resY)
+	Canvas:SetParent(SceneCanvas)
+
+	local self = setmetatable({
+		screenGui = SceneGui,
+		canvas = Canvas,
+		camera = nil
+	}, Scene)
+
+	return self
 end
 
 return Scene
--- Engine/init.lua
+++ Engine/init.lua
@@ -2,7 +2,6 @@ local Core = script.Core
 local Common = script.Common
 
 local Scene = require(script.Scene)
-local ViewportCanvas = require(Common.ViewportCanvas)
 
 local Engine = {}
 Engine.__index = Engine
@@ -14,8 +13,8 @@ function Engine.new()
 	}, Engine)
 end
 
-function Engine.NewScene(sceneName: string)
-	return Scene.new(sceneName, true)
+function Engine.NewScene(resX: number, resY: number, sceneName: string?)
+	return Scene.new(resX, resY, sceneName, true)
 end
 
 function Engine:DestroyAllScenes()
